6c7bc934f246963310a8459da368a1d8cea6288f
thatikondasathwik <sathwikthatikonda5@gmail.com>
2026-06-19 19:39:39 +0530

    fix: terraform backend region to ap-south-1

--- terraform/version.tf
+++ terraform/version.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "employee-leave-1"
     key     = "state/terraform.tfstate"
-    region  = "us-east-1"
+    region  = "ap-south-1"
     encrypt = true
   }
 }
